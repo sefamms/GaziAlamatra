9009db53146b9f51aedc09263e2248ec7081a41f
sefamms <smemis412@gmail.com>
2021-02-23 20:21:39 +0300

    Add project files.

---
+++ AlamatraWEBUI/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using AlamatraWEBUI.Models;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlamatraWEBUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IEmailSender _emailsender;
+
+        public HomeController(ILogger<HomeController> logger,IEmailSender emailsender)
+        {
+            _logger = logger;
+            this._emailsender = emailsender;
+        }
+            
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Ekibimiz()
+        {
+            return View();
+        }
+
+        public IActionResult Hakkimizda()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ContactEmail()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> ContactEmail(Contact contact)
+        {
+            var msg = contact.Name + " " + contact.Message;
+            await _emailsender.SendEmailAsync(contact.Email, contact.Subject, msg);
+            ViewBag.ConfirmMsg = "Thanks for Your Mail";
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AlamatraWEBUI/Repository/SendEmail.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace AlamatraWEBUI.Repository
+{
+    public class SendEmail : IEmailSender
+    {
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+           using(MailMessage mailMessage=new MailMessage())
+            {
+                mailMessage.From = new MailAddress("");
+                mailMessage.Subject = subject;
+                mailMessage.Body = email + htmlMessage;
+                mailMessage.IsBodyHtml = true;
+                mailMessage.To.Add(new MailAddress(""));
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.EnableSsl = true;
+                System.Net.NetworkCredential NetworkCred = new System.Net.NetworkCredential();
+                NetworkCred.UserName = "lukas.poldi51@gmail.com";
+                NetworkCred.Password = "S.memis412";
+                smtp.UseDefaultCredentials = true;
+                smtp.Credentials = NetworkCred;
+                smtp.Port = 25;
+                await smtp.SendMailAsync(mailMessage);
+            }
+        }
+    }
+}
